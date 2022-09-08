c8d22fbcc17ed5222e720cb194baa383d1635bdb
Cristiandgr <cristiandgr24@gmail.com>
2022-09-08 21:51:04 +0100

    feat: add news components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import { Routes, Route } from "react-router";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
-  const Shop = () => {
-    return <div>Shop</div>;
-  };
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
